9484e265f217c9fdf88dfcd4ff1eda7a720ba564
yplam <yplam@ca.ibm.com>
2019-05-11 16:52:29 -0400

    Setup of fact and dimension tables

--- Data Modeling Postgres/project/sql_queries.py
+++ Data Modeling Postgres/project/sql_queries.py
@@ -1,26 +1,72 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP table IF EXISTS songplays"
+user_table_drop = "DROP table IF EXISTS users"
+song_table_drop = "DROP table IF EXISTS songs"
+artist_table_drop = "DROP table IF EXISTS artists"
+time_table_drop = "DROP table IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id serial, 
+        start_time timestamp, 
+        user_id int NOT NULL, 
+        level varchar, 
+        song_id int, 
+        artist_id int, 
+        session_id int, 
+        location varchar, 
+        user_agent varchar,
+        PRIMARY KEY (songplay_id)        
+        );
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id int NOT NULL,
+        first_name varchar,
+        last_name varchar,
+        gender varchar,
+        level varchar,
+        PRIMARY KEY (user_id)
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id varchar NOT NULL,
+        title varchar,
+        artist_id varchar,
+        year int,
+        duration decimal,
+        PRIMARY KEY (song_id)
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id varchar NOT NULL,
+        name varchar,
+        location varchar,
+        latitude decimal,
+        longitude decimal,
+        PRIMARY KEY (artist_id)
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time timestamp NOT NULL,
+        hour int,
+        day int,
+        week int,
+        month int,
+        year int,
+        weekday int,
+        PRIMARY KEY (start_time)
+    );
 """)
 
 # INSERT RECORDS
