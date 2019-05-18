75aeb70efe1ae063fca83e86fd2dd2f998c10112
yplam <yplam@ca.ibm.com>
2019-05-18 14:54:46 -0400

    Add print statements to follow progress

--- Cloud Data Warehouses/project/create_tables.py
+++ Cloud Data Warehouses/project/create_tables.py
@@ -5,14 +5,14 @@ from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
-        print('Query ', query)
+        print('Drop table query ', query)
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
     for query in create_table_queries:
-        print('Query ', query)
+        print('Create table query ', query)
         cur.execute(query)
         conn.commit()
 
--- Cloud Data Warehouses/project/etl.py
+++ Cloud Data Warehouses/project/etl.py
@@ -5,12 +5,14 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
+        print('Copy query ', query)
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
+        print('Running insert query ', query)
         cur.execute(query)
         conn.commit()
 
