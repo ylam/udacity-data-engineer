107bd5b89e4643a11bf65d779ad1eb509c0c1d7f
yplam <yplam@ca.ibm.com>
2019-05-30 20:01:33 -0400

    Add DAG configurations

--- Data Pipeline Airflow/project/airflow/dags/udac_example_dag.py
+++ Data Pipeline Airflow/project/airflow/dags/udac_example_dag.py
@@ -12,12 +12,17 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False,
 }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='0 * * * *',
+          catchup=False
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
